99c9bf15dc305b912f27b710c688eb0a9821cc26
BrookeGibbons <21301367@student.uwa.edu.au>
2018-08-31 13:57:42 +0800

    a lot of changes

--- DEMO_get_campaigns_by_workgroup.R
+++ DEMO_get_campaigns_by_workgroup.R
@@ -17,11 +17,26 @@ eval(parse(text = galib))
 functions <-getURL("https://raw.githubusercontent.com/GlobalArchiveManual/globalarchive-query/master/Functions.R", ssl.verifypeer = FALSE)
 eval(parse(text = functions))
 
+### Set your working directory ----
+working.dir<-("C:/GitHub/globalarchive-query") # This is the only folder you will need to create outside of R
+setwd(working.dir)
+
+## Save directory names ----
+download.dir<-paste(working.dir,"Downloads",sep="/")
+tidy.dir<-paste(working.dir,"Tidy data",sep="/")
+
+unlink(download.dir, recursive=TRUE) # Clear downloads folder (this will delete everything in the downloads folder (very scary)), DO NOT save anything in this file that is not downloaded using the query as it will be deleted and is not recoverable
+
+## Create a folder for downloaded data and tidy data ----
+dir.create(file.path(working.dir, "Downloads"))
+dir.create(file.path(working.dir, "Tidy data"))
+
 ### Setup your query ----
+# API
 API_USER_TOKEN <- "ef231f61b4ef204d39f47f58cccadf71af250a365e314e83dbcb3b08"  # Change to demo user when received
 
 # This is the location where the downloaded data will sit ----
-DATA_DIR <- "WorkgroupQuery"
+DATA_DIR <- download.dir
 
 # Configure search pattern for downloading all files ----
 MATCH_FILES <- ".csv$|.txt$"
@@ -29,70 +44,80 @@ MATCH_FILES <- ".csv$|.txt$"
 # API search by Collaboration/Workgroup (space replaced with +) ----
 q='{"filters":[{"name":"workgroups","op":"any","val":{"name":"name","op":"eq","val":"Example:+merging+different+data+types"}}]}'
 
+# Name of the collaboration to save files at the end ----
+name<-"Example merging different data types"
+
 ### Run the query and process the campaigns. Files will be downloaded into DATA_DIR ----
 nresults <- ga.get.campaign.list(API_USER_TOKEN, process_campaign_object, q=q)
 
 ## Annotation info ----
-info<-list.files(path="WorkgroupQuery",
+info<-list.files(path=download.dir,
                  recursive=T,
                  pattern="_info.csv",
                  full.names=T) %>% 
   map_df(~read_files_csv(.))%>%
-  tidyr::separate(campaign.naming,into=c("folder","project","campaignid","file"),sep="/")%>%
   select(campaignid,name,value)%>%
   tidyr::spread(name,value)
 
 ## Metadata files ----
-metadata <-list.files(path="WorkgroupQuery",
+metadata <-list.files(path=download.dir,
                       recursive=T,
                       pattern="Metadata.csv",
                       full.names=T) %>% 
   map_df(~read_files_csv(.))%>%
-  tidyr::separate(campaign.naming,into=c("folder","project","campaignid","file"),sep="/")%>%
-  dplyr::select(campaignid,sample,latitude,longitude,date,time,location,status,site,depth,observer,successful.count,successful.length,comment)%>%
+  dplyr::select(project,campaignid,sample,latitude,longitude,date,time,location,status,site,depth,observer,successful.count,successful.length,comment)%>%
   left_join(info)%>% # Join in annotation info
   glimpse()
 
-## EventMeasure exports ----
+## Eventmeasure files ----
 ## Points files ----
-points <-list.files(path="WorkgroupQuery",
+points <-list.files(path=download.dir,
                     recursive=T,
                     pattern="_Points.txt",
                     full.names=T) %>% 
   map_df(~read_files_txt(.))%>%
-  tidyr::separate(campaign.naming,into=c("folder","project","campaignid","file"),sep="/")%>%
   dplyr::rename(sample=opcode)%>%
   dplyr::select(campaignid,sample,family,genus,species,number,frame)%>%
   glimpse()
 
 ## 3D Points files ----
-threedpoints <-list.files(path="WorkgroupQuery",
+threedpoints <-list.files(path=download.dir,
                           recursive=T,
                           pattern="3DPoints.txt",
                           full.names=T) %>%
   map_df(~read_files_txt(.))%>%
-  tidyr::separate(campaign.naming,into=c("folder","project","campaignid","file"),sep="/")%>%
   dplyr::rename(sample=opcode)%>%
   dplyr::select(campaignid,sample,family,genus,species,range,number,comment)%>%
   glimpse()
 
 ## Lengths files ----
-lengths <-list.files(path="WorkgroupQuery",
+lengths <-list.files(path=download.dir,
                      recursive=T,
                      pattern="Lengths.txt",
                      full.names=T) %>% 
   map_df(~read_files_txt(.))%>%
-  tidyr::separate(campaign.naming,into=c("folder","project","campaignid","file"),sep="/")%>%
   dplyr::rename(sample=opcode)%>%
   dplyr::select(campaignid,sample,family,genus,species,length,range,number,comment)%>%
   glimpse()
 
 ### Generic Campaigns ----
 ## Count fles ----
-
+count <-list.files(path=download.dir,
+                     recursive=T,
+                     pattern="Count.csv",
+                     full.names=T) %>% 
+  map_df(~read_files_csv(.))%>%
+  dplyr::select(campaignid,sample,family,genus,species,count)%>%
+  glimpse()
 
 ## Length files ----
-
+length <-list.files(path=download.dir,
+                     recursive=T,
+                     pattern="Length.csv",
+                     full.names=T) %>% 
+  map_df(~read_files_csv(.))%>%
+  dplyr::select(campaignid,sample,family,genus,species,length,count)%>%
+  glimpse()
 
 ## Make Maxn and length dataframes----
 maxn<-points%>%
@@ -103,18 +128,29 @@ maxn<-points%>%
   slice(which.max(maxn))%>%
   ungroup()%>%
   filter(!is.na(maxn))%>%
-  filter(!maxn==0)%>%
+  dplyr::select(-frame)%>% 
+  plyr::rbind.fill(count)%>% # Add in generic count data
   inner_join(metadata)%>%
-  filter(successful.count=="Yes")%>%
+  replace_na(list(maxn=0))%>%
+  dplyr::filter(successful.count=="Yes")%>%
+  dplyr::mutate(maxn=ifelse(maxn%in%c(0,NA),count,maxn))%>% 
+  dplyr::mutate(maxn=as.numeric(maxn))%>%
+  dplyr::select(-c(count))%>%
+  filter(maxn>0)%>%
   glimpse()
 
 length3dpoints<-lengths%>%
   plyr::rbind.fill(threedpoints)%>%
+  plyr::rbind.fill(length)%>%
+  dplyr::mutate(number=ifelse(number%in%c(0,NA),count,number))%>%
+  dplyr::select(-c(count))
   dplyr::mutate(length=as.numeric(length))%>%
   dplyr::mutate(number=as.numeric(number))%>%
   inner_join(metadata)%>%
   filter(successful.length=="Yes")%>%
   glimpse()
-
-
-
+  
+## Save maxn and length files ----
+setwd(tidy.dir)
+write.csv(maxn,paste(name,"maxn.csv",sep="_"),row.names = FALSE)
+write.csv(length3dpoints,paste(name,"length3dpoints.csv",sep="_"),row.names = FALSE)
