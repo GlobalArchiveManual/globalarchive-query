9ba8fef8e31647676d6ed0e721f1ba1dfbae8489
BrookeGibbons <21301367@student.uwa.edu.au>
2019-03-28 10:25:51 +0800

    moved functions to Ga package

--- values.R
+++ values.R
@@ -9,31 +9,3 @@ DATA_DIR <- download.dir
 
 # Configure search pattern for downloading all files ----
 MATCH_FILES <- ".csv$|.txt$"
-
-
-# New functions here for the moment
-list.files.GA<-function(pattern) {
-  list.files(path=download.dir, # create a list of files that match the pattern "_Points.txt"
-             recursive=T,
-             pattern=pattern,
-             full.names=T)
-}
-
-
-expand.files<-function(files) {
-  as.data.frame(files)%>%
-    mutate(campaign=row.names(.))%>%
-    filter(lines>1)%>% # filter out all empty text files
-    select(campaign)%>%
-    as_vector(.)
-}
-
-
-read_files_txt <- function(flnm) {
-  read_tsv(flnm,col_types = cols(.default = "c"))%>% 
-    mutate(campaign.naming=str_replace_all(flnm,paste(download.dir,"/",sep=""),""))%>%
-    tidyr::separate(campaign.naming,into=c("project","campaignid"),sep="/",remove=TRUE)%>%
-    clean_names%>%
-    plyr::rename(., replace = c(opcode="sample"),warn_missing = TRUE)
-}
-
